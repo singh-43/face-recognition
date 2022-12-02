4a0bb3d5ebdf5b31c488ddbd8b509e024ca13a4d
singh-43 <jatinsinghjs03@gmail.com>
2022-12-03 02:38:52 +0530

    changing logo files

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import robot from './robot.png'
 import Tilt from 'react-parallax-tilt';
-import './ParallaxEffectImg.demozap.scss';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
-	return(
-		<div>
-		  <Tilt
-			    className="br2 shadow-2 parallax-effect-img"
-			    tiltMaxAngleX={40}
-			    tiltMaxAngleY={40}
-			    perspective={800}
-			    transitionSpeed={1500}
-			    scale={1.1}
-			    gyroscope={true}
-			    style={{height:150,width:150}}
-			  >
-			  <div className='pa3 tc align-items'><img style={{paddingTop: '5px'}} alt='logo' src={robot} /></div>
-		  </Tilt>
-		</div>
-	)
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+        </div>
+      </Tilt>
+    </div>
+  );
 }
 
 export default Logo;
